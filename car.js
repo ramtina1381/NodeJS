ce6d4f442f7d320073bf0c8599fc78d371e28ca2
Ramtin Abolfazli <Ramtina1381@gmail.com>
2024-10-05 18:58:25 -0400

    Node js tutorial

---
+++ car.js
@@ -0,0 +1,13 @@
+exports.ford = {
+    brand: "Ford",
+    model: "Fiesta"
+}; 
+
+exports.tesla = {
+    brand: "Tesla",
+    model: "Model 3"
+}
+
+// module.exports = ford;
+// now i can make use of this car in other files as needed. 
+// exports.data = { ford, tesla };
---
+++ index.js
@@ -0,0 +1,8 @@
+// const _ = require("lodash")
+// const arr = [1, 4, 6, 8];
+
+// console.log(_.chunk(arr)) 
+
+const {ford, tesla} = require("./car")
+console.log(ford)
+console.log(tesla)
